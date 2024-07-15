f29fa72fea7b3f182384bda208a63b39763f60b2
Peter Nsaka <peternsaka@Peters-MacBook-Pro.local>
2024-07-15 09:48:17 -0400

    programmer oop

--- openclassroom_1/programmer_oop/step.cpp
+++ openclassroom_1/programmer_oop/step.cpp
@@ -1,1 +1,14 @@
 #include "step.hpp"
+
+using namespace std;
+
+/*=================================== utiliser les classes ======================================*/
+
+int main(void)
+{
+	string	test_string;
+
+	test_string = "helloworld";
+	cout << test_string << endl;
+}
+
