4ef9b712ae083d453839b056fe38b3be98047cfc
Peter Malonge Nsaka <pnsaka@c1r8p8.42quebec.com>
2024-08-23 14:55:49 -0400

    avant push

---
+++ CPPXX/CPP02/ex00/Fixed.hpp
@@ -0,0 +1,19 @@
+#ifndef DEF_FIXED_HPP
+#define DEF_FIXED_HPP
+
+#include <string>
+#include <iostream>
+
+class Fixed
+{
+    public:
+        Fixed(/* args */);
+        Fixed(/* args */);
+        Fixed(/* args */);
+        ~Fixed();
+
+    private:
+        /* data */
+};
+
+#endif
