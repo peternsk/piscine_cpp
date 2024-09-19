bb72ba53bb9d0cb844f75ec7eabff590bf32de5c
Peter Malonge Nsaka <pnsaka@c1r7p8.42quebec.com>
2024-09-18 21:47:44 -0400

    module 5 ex01

--- CPPXX/CPP05/ex01/Form.cpp
+++ CPPXX/CPP05/ex01/Form.cpp
@@ -6,21 +6,51 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 14:15:06 by pnsaka            #+#    #+#             */
-/*   Updated: 2024/09/18 14:25:27 by pnsaka           ###   ########.fr       */
+/*   Updated: 2024/09/18 21:37:02 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+#pragma once
+#include "Form.hpp"
 
-class Form {
-    public:
-        Form();
-        Form(const Form& cpy);
-        Form& operator=( const Form& objForm);
-        
-        ~Form();
-    private:
-        std::string name;
-        bool 
+
+/******************************************************************************/
+/**** CONSTROCTOR & DESTRUCTOR  ***********************************************/
+/******************************************************************************/
+
+Form::Form() : name(""), gradeToSign(0), gradeToExecute(0){
+    if(name.empty() == 1 || gradeToSign == 0 || gradeToExecute == 0)
+        throw Form::CannotInitForm();
 }
+
+Form::Form(Form& const cpy){
+    *this = cpy;
+}
+
+Form::Form(std::string m_name, int m_gradeSign, int m_gradeExecute) : name(m_name), gradeToSign(m_gradeSign), gradeToExecute(m_gradeExecute){
+    if(this->name.empty() == 1)
+        throw Form::CannotInitForm();
+    if(this->gradeToExecute < 1 || this->gradeToSign < 1)
+        throw Form::GradeTooHighException();
+    if(this->gradeToExecute > 150 || this->gradeToSign > 150)
+        throw Form::GradeTooLowException();
+}
+
+Form::~Form(){}
+
+/******************************************************************************/
+/****   OVERLOADING OPERATOR    ***********************************************/
+/******************************************************************************/
+
+Form& Form::operator=( const Form& objForm);
+
+
+
+/******************************************************************************/
+/****          METHODS          ***********************************************/
+/******************************************************************************/
+
+
+/******************************************************************************/
+/****         FUNCTIONS         ***********************************************/
+/******************************************************************************/
--- CPPXX/CPP05/ex01/Form.hpp
+++ CPPXX/CPP05/ex01/Form.hpp
@@ -1,1 +1,51 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <stdbool.h>
+#include "Bureaucrat.hpp"
+
+
+class Form {
+    public:
+        Form();
+        Form(const Form& cpy);
+        Form(std::string m_name, int m_gradeSign, int m_gradeExecute);
+
+        Form& operator=( const Form& objForm);
+        
+        ~Form();
+
+        std::string getName() const;
+        std::string getGradeToSign() const;
+        std::string getGradeToExecute() const;
+        
+        void beSigned(Bureaucrat &Bur);
+        class GradeTooHighException : public std::exception{
+            public:
+                virtual const char* what() const throw(){
+                    return "ERROR : Grade too high";
+                }
+        };
+
+        class GradeTooLowException : public std::exception{
+            public:
+                virtual const char* what() const throw(){
+                    return "ERROR : Grade too low";
+                }
+        };
+        
+         class CannotInitForm : public std::exception{
+            public:
+                virtual const char* what() const throw(){
+                    return "ERROR : Attribute aren't initialize proprely...";
+                }
+        };
+
+    private:
+        std::string const name;
+        bool    formSigned;
+        int     const gradeToSign;
+        int     const gradeToExecute;
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& obj);
